a33e275d9531dee13281d4da9adc68cd7b56896c
manual-eel <207329734+manual-eel@users.noreply.github.com>
2025-07-01 22:46:51 +0200

    add option to hide virtual text when virtual lines shown

--- lua/virtline-toggle/init.lua
+++ lua/virtline-toggle/init.lua
@@ -1,13 +1,22 @@
 local M = {}
 
+local defaults = {
+	invert_virtual_text = false,
+}
+
 M.show_virtual_lines = false
 
 function M.toggle()
 	M.show_virtual_lines = not M.show_virtual_lines
-	vim.diagnostic.config({ virtual_lines = M.show_virtual_lines })
+	local new_config = { virtual_lines = M.show_virtual_lines }
+	if M.opts.invert_virtual_text then
+		new_config.virtual_text = not M.show_virtual_lines
+	end
+	vim.diagnostic.config(new_config)
 end
 
-function M.setup()
+function M.setup(opts)
+	M.opts = vim.tbl_extend("force", defaults, opts or {})
 	vim.api.nvim_create_user_command("VirtLineToggle", M.toggle, {})
 end
 
